0d93df2a6317c541937a2592ca573ed5430dcdaa
zl3580 <35804583@163.com>
2020-05-26 11:26:32 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-npm run stop
-git reset --hard origin/master
-git clean -f
-git pull
-npm run tsc
-npm run start
+
+echo('sh  成功了')
+# npm run stop
+# git reset --hard origin/master
+# git clean -f
+# git pull
+# npm run tsc
+# npm run start
