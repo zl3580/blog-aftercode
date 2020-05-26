2b20fd4541031ef5ff66b9818c491bf0df87717b
zl3580 <zxc35804583@163.com>
2020-05-26 21:51:05 +0800

    commit

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -2,7 +2,7 @@ import { Controller } from 'egg';
 
 export default class HomeController extends Controller {
   public async index() {
-    console.log('测试');
+    console.log('测试klk;lk;lk;l');
     const { ctx } = this;
     ctx.body = await ctx.service.test.sayHi(ctx.request.body);
   }
