304ec8b84eec8000888bd8a37acb38d6b8d17e5a
zl3580 <35804583@163.com>
2020-05-27 11:21:14 +0800

    commit

--- deploy.js
+++ deploy.js
@@ -16,7 +16,7 @@ http.createServer(function(req, res) {
     res.end('no such location');
   });
 
-}).listen(3000);
+}).listen(7777);
 
 handler.on('error', function(err) {
   console.error('Error----------:', err.message);
