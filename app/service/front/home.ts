1cd5b96f5a45c4c18ed9c23960881760a934d031
zl3580 <35804583@163.com>
2020-06-24 17:59:24 +0800

    [commit]

--- app/service/front/home.ts
+++ app/service/front/home.ts
@@ -17,15 +17,21 @@ export default class HomeService extends Service {
     const {
       ctx,
     } = this;
-    const result = await ctx.model.Photo.aggregate([{ $match: { status: 1 } }, { $sample: { size: 5 } }]);
-    console.log('ArticleService -> getPhoto -> result', result);
-    const data = result.map(item => {
-      return {
-        address: item.address,
-        img: [ item.imgs[0], item.imgs[1] ],
-        _id: item._id,
-      };
-    });
-    return data;
+    // promise
+    // const result = new Promise(resolve => {
+    //   ctx.model.PhotoList.aggregate([{ $match: { status: 1 } }, { $sample: { size: 10 } }], function(error, res1) {
+    //     console.log('error', error);
+    //     ctx.model.PhotoList.populate(res1, { path: 'photoContent', select: 'address introduce' }, function(error, res2) {
+    //       console.log('HomeService -> getPhoto -> error', error);
+    //       resolve(res2);
+    //     });
+    //   });
+    // });
+    // return result;
+
+
+    const result = await ctx.model.PhotoList.aggregate([{ $match: { status: 1 } }, { $sample: { size: 10 } }]);
+    const result1 = await ctx.model.PhotoList.populate(result, { path: 'photoContent', select: 'address introduce' });
+    return result1;
   }
 }
